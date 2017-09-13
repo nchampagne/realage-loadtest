1e198f67515f524c98a7dedcec6fd41712b131bd
Nick Champagne <nchampagne@sharecare.com>
2017-09-13 18:04:23 -0400

    Refactored http class.

--- src/main/scala/com/sharecare/http/Http.scala
+++ src/main/scala/com/sharecare/http/Http.scala
@@ -5,16 +5,14 @@ import java.util.concurrent.TimeUnit
 import akka.actor.{ActorSystem, Terminated}
 import akka.stream.ActorMaterializer
 import com.sharecare.error.RealAgeError
-import play.api.libs.json.{Format, JsValue}
+import play.api.libs.json.{Format, JsValue, Json}
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.Duration
-
 import play.api.libs.ws.JsonBodyReadables._
 import play.api.libs.ws.JsonBodyWritables._
-
-import com.sharecare.serialization.JsonFormats._
+import play.api.libs.ws.StandaloneWSRequest
 
 object Http {
 
@@ -24,16 +22,24 @@ object Http {
   private lazy val wsClient       = StandaloneAhcWSClient()
 
   def GET[T](url: String, headers: (String, String)*)(implicit f: Format[T], ec: ExecutionContext): Future[Either[RealAgeError, Option[T]]] = {
-    wsClient.url(url).withHttpHeaders(headers: _*).get().map { response =>
-      response.status match {
-        case 200  => Right(response.body[JsValue].asOpt[T])
-        case _    => Left(response.body[JsValue].as[RealAgeError])
-      }
-    }
+    wsClient.url(url).withHttpHeaders(headers: _*).get().map(toResponse(_))
+  }
+
+  def POST[A, B](body: A, url: String, headers: (String, String)*)(implicit f0: Format[A], f1: Format[B], ec: ExecutionContext) = {
+    wsClient.url(url).withHttpHeaders(headers: _*).post(Json.toJson(body)).map(toResponse[B](_))
   }
 
   def close(): Terminated = {
     wsClient.close()
     Await.result(system.terminate(), Duration(10, TimeUnit.SECONDS))
+  }
+
+  // TODO - Move to implicit class
+  private def toResponse[T](response:  StandaloneWSRequest#Response)(implicit f: Format[T]): Either[RealAgeError, Option[T]] = {
+    import com.sharecare.serialization.JsonFormats.RealAgeError_JsonFormat
+    response.status match {
+      case 200  => Right(response.body[JsValue].asOpt[T])
+      case _    => Left(response.body[JsValue].as[RealAgeError])
+    }
   }
 }
